629eb98493cb1c41c8a71a1270fa8441f8298a17
RedNotSus <Jiayangc1@gmail.com>
2023-04-06 18:42:28 +0000

    seo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ export const headerData = {
   name: "Jiayang Chen",
   title: "Hello! I am",
   desciption:
-    "My name is Jiayang Chen, and I am a high school student with a passion for coding.",
+    "My name is Jiayang Chen, and I am a high school student with a passion for coding. ",
   image: "https://55gms.tk/img/pfp.jpg",
   imagebw: "https://55gms.me/img/ji-modified.jpeg",
   resumePdf: "https://rednotsus.tk/assets/resume.pdf",
