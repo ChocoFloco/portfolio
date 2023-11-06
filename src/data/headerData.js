35cda5fc5a1a00c8cbb3c689b76e8524aca4cd17
Jiayang Chen <Jiayangc1@gmail.com>
2023-11-05 20:05:11 -0600

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ export const headerData = {
   name: "Jiayang Chen",
   title: "Hello! I am",
   desciption:
-    "My name is Jiayang Chen, and I am a high school student with a passion for coding. ",
-  image: "https://55gms.tk/img/pfp.jpg",
+    "My name is Jiayang Chen, and I am a student with a passion for coding. ",
+  image: "https://55gms.me/img/pfp.jpg",
   imagebw: "https://55gms.me/img/ji-modified.jpeg",
   resumePdf: "https://rednotsus.tk/assets/resume.pdf",
 };
