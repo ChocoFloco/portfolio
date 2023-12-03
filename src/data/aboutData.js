d52062d6deb2e006aaf1ff0f8b442a2f0d854670
ChocoFloco <149216961+ChocoFloco@users.noreply.github.com>
2023-12-02 18:58:09 -0600

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name is Jiayang Chen, and I am a high school student with a passion for coding Discord bots. As an individual with a keen interest in computer programming, I am writing this piece to share my journey towards developing proficiency in this field.",
+    "My name is Rishi Ruparelia, and I am a high school student with a passion for coding Discord bots. As an individual with a keen interest in computer programming, I am writing this piece to share my journey towards developing proficiency in this field.",
   description2:
     "My journey began at the age of eight when I stumbled upon a YouTube video about Python coding. The concept of creating custom text on a screen with a few lines of code fascinated me, and I was immediately hooked. I devoted my time and effort to learning Python, and subsequently, I gained a comprehensive understanding of Javascript. After a brief hiatus, I further expanded my knowledge by learning node.js and the basics of discord.js, allowing me to develop and code Discord bots. I, like many others, aspire to pursue a career in backend development, and my ultimate goal is to attend the prestigious Massachusetts Institute of Technology (MIT) to achieve this. My journey has taught me the importance of perseverance and dedication, and I hope that my experience inspires others to pursue their passions.",
 };
